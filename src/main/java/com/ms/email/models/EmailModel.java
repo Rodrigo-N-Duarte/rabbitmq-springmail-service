dafbcc3c60f8ae8274215643ac5848317d4400f3
Israel Alexsander <rael3014@gmail.com>
2024-05-11 12:26:30 -0300

    Microservice de email

---
+++ src/main/java/com/ms/email/models/EmailModel.java
@@ -0,0 +1,28 @@
+package com.ms.email.models;
+
+import com.ms.email.enums.StatusEmail;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+
+@Data
+@Entity
+@Table(name = "TB_EMAIL")
+public class EmailModel implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long emailId;
+    private String ownerRef;
+    private String emailFrom;
+    private String emailTo;
+    private String subject;
+    @Column(columnDefinition = "Text")
+    private String text;
+    private LocalDateTime sendDate;
+    private StatusEmail statusEmail;
+}
